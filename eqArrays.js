bfe7af6552728cca7a93c40061284f057d7f8c49
Connie <leungcnie@gmail.com>
2020-10-27 21:57:09 +0000

    Created function that checks if 2 arrays are identical

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE: all should pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false); 
+assertEqual(eqArrays([], []), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 4, 5]), false);
+assertEqual(eqArrays(["a", "b"], ["a", "b"]), true);
+assertEqual(eqArrays([1, 2, 3], []), false);
