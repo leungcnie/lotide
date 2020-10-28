e5a9b9c545372fede5196b8503dfd2575e7e92a4
Connie <leungcnie@gmail.com>
2020-10-28 21:47:23 +0000

    Wrote letterPositions function that returns an array of indices that each character in a string appears at

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] is equal to [${arr2}]`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] is not equal to [${arr2}]`);
+  }
+}
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence.charAt(i)]) {
+      results[sentence.charAt(i)].push(i);
+    } else {
+      results[sentence.charAt(i)] = [i];
+    }
+  }
+  return results;
+}
+
+// TEST CODE
+const example = letterPositions("hello a");
+assertArraysEqual(example["h"], [0]);
+assertArraysEqual(example["e"], [1]);
+assertArraysEqual(example["l"], [2, 3]);
+assertArraysEqual(example["o"], [4]);
+assertArraysEqual(example["a"], [6]);
