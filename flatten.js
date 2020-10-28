c8963be92f7eb3e940e9a13bdf3d39d5ba9fa31b
Connie <leungcnie@gmail.com>
2020-10-28 03:36:31 +0000

    Refactored code so that it would account for all data types

--- flatten.js
+++ flatten.js
@@ -1,21 +1,34 @@
 const flatten = (array) => {
   let output = [];
-  let strings = "";
   array.forEach(item => {
     if (Array.isArray(item)) {
-      strings += item.toString() + ",";
+      item.forEach(element => output.push(element));
     } else {
-      strings += item + ",";
+      output.push(item);
     }
   })
-  strings = strings.split(",").slice(0, -1);
-  strings.forEach(str => {
-    if (str !== "") {
-      output.push(Number(str));
-    }
-  });
   return output;
 }
+
+// FIRST ATTEMPT: tried to avoid using 2 nested loops but got too messy 
+// const flatten = (array) => {
+//   let output = [];
+//   let strings = "";
+//   array.forEach(item => {
+//     if (Array.isArray(item)) {
+//       strings += item.toString() + ",";
+//     } else {
+//       strings += item + ",";
+//     }
+//   })
+//   strings = strings.split(",").slice(0, -1);
+//   strings.forEach(str => {
+//     if (str !== "") {
+//       output.push(Number(str));
+//     }
+//   });
+//   return output;
+// }
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
@@ -38,3 +51,5 @@ const eqArrays = (arr1, arr2) => {
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
 assertArraysEqual(flatten([[]]), []);
 assertArraysEqual(flatten([1, 2, [], 3]), [1, 2, 3]);
+assertArraysEqual(flatten(["a", "b", [], "c"]), ["a", "b", "c"]);
+assertArraysEqual(flatten(["a", true, 1, [false, 2]]), ["a", true, 1, false, 2]);
