0c7ec71fe50e82b57d2f1d62c039f977fd8bc4a2
Connie <leungcnie@gmail.com>
2020-10-29 01:39:58 +0000

    Added missing return keyword for assertEqualArrays functions

--- flatten.js
+++ flatten.js
@@ -32,7 +32,7 @@ const flatten = (array) => {
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+    return console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
   } else {
     return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
   }
--- middle.js
+++ middle.js
@@ -13,7 +13,7 @@ const eqArrays = (arr1, arr2) => {
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+    return console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
   } else {
     return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
   }
