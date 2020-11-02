a2839cba8c2d25e3ee51627e1e614b514d345e00
Connie <leungcnie@gmail.com>
2020-11-02 20:19:00 +0000

    Refactored tail function and put its test code in a separate file

--- tail.js
+++ tail.js
@@ -1,28 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-// TEST CODE
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// Check if original array is modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // original array should still have 3 elements!
-// 1 element array
-const one = ["nyeh"];
-assertEqual(tail(one).length, 0); // should be empty
-// Empty array
-const empty = [];
-assertEqual(tail(empty).length, 0);
+module.exports = tail;
