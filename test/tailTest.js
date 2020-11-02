3a00a92b6981fb9c3925fe01b04b9dc23e0460f3
Connie <leungcnie@gmail.com>
2020-11-02 21:33:36 +0000

    Set up tailTest.js to use M & C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert.deepEqual;
 const tail = require('../tail');
 
-// TEST CODE
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// Check if original array is modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // original array should still have 3 elements!
-// 1 element array
-const one = ["nyeh"];
-assertEqual(tail(one).length, 0); // should be empty
-// Empty array
-const empty = [];
-assertEqual(tail(empty).length, 0);
+describe("#tail", () => {
+
+})
+// // TEST CODE
+// // Test Case 1: Check the returned array elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// // Check if original array is modified
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// // 1 element array
+// const one = ["nyeh"];
+// assertEqual(tail(one).length, 0); // should be empty
+// // Empty array
+// const empty = [];
+// assertEqual(tail(empty).length, 0);
